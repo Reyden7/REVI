63e0e3dc28bb03440c0daeb146d0e26be068f1c2
quent <quentin.cordiero@neuf.fr>
2021-11-09 17:06:45 +0100

    first commit nav bar, include boostrap et jquery include meterials organisation

---
+++ src/homepage.php
@@ -0,0 +1,63 @@
+<?php include "inc/header.php"; ?>
+
+<html>
+    <body>
+    <div class="container ">
+        <div class="row">
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg" class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg" class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg" class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg" class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+            <div class="col-4 h-auto mt-5 ">
+                <img src="img/img_logo_blue.jpg"class="img-fluid" />
+            </div>
+
+        </div>
+    </div>
+
+
+    </body>
+</html>
+
+<?php include "inc/footer.php"; ?>
