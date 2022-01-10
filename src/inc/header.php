01fc9f2a2e62dcaf74d7bdcf798cb0845f83fa8d
Reyden7 <quentin.cordiero@neuf.fr>
2022-01-10 19:50:19 +0100

    reparation

--- src/inc/header.php
+++ src/inc/header.php
@@ -19,7 +19,7 @@
     <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="http://127.0.0.1/JuniorAgence/src/js/anime.min.js">
+    <script src="http://127.0.0.1/JuniorAgence/src/anime.min.js"></script>
     
 
     <!DOCTYPE html>
