abcd2112043b899b69db00a89314589256d008e6
Reyden <quentin.cordiero@neuf.fr>
2022-04-25 22:45:36 +0200

    refonte
    video background, + content

--- src/view_homepage.php
+++ src/view_homepage.php
@@ -8,39 +8,120 @@
     </div>
 </div>
 
-<video class="w-100" autoplay  class="demoVideo"  muted loop>
-  <source src="/src/recherche.mp4" type="video/mp4">
-Your browser does not support the video tag.
-</video>
-
-
-<input type="button" value="" onClick="AfficherMasquer()"  style="background:transparent;border:none ; width:20px"/>
-            <script type="text/javascript">
-                function AfficherMasquer()
-                {
-                divInfo = document.getElementById('divacacher');
-                var audio = new Audio('audio/audio.mp3').play()
-                
-                if (divInfo.style.display == 'none')
-                divInfo.style.display = 'block';
-                
-                
-                else
-                divInfo.style.display = 'none';
-                
-                }
-                
-              </script>
-               
-            
-                <div id="divacacher" style="display:none;">             
-                    <img src="img/tope.png"  alt="profile-image" class=" img-fluid w-50 h-50" style="z-index:1"/>
-                </div>
-</input>
-
-
      
+<!-- The video -->
+<div class="bloc">
+  <video src="video1.mp4" autoplay loop muted></video>
+  
+  </div>
 
-    
+
+<div class="container backgroundBande "  style="margin-top: -80px;">
+  <div class="row-full shadow mt-0">
+      <span class="text-center "> 
+        
+          <div class="col-12 pt-4 ">
+          
+            <h1 class="fontbandeau" >Le master REVI</h1>
+            
+          </div>
+        </div>
+      
+      </span>
+    </div>
+</div>
+
+<div class="container">
+  <div class="row">
+  <h2 class="mt-5">Ce master comprend deux parcours, un parcours Ingénierie de la Recherche et un parcours Veille Internationale. Il s’adresse à des étudiants diplômés en langue étrangère (allemand, anglais, espagnol, niveau licence LLCER ou équivalent. Le niveau requis en français: C2. Débouchés: – chargé de veille, analyste en e-réputation, analyste de données d’opinion – enseignant, enseignant-chercheur, travail dans l’administration de la recherche.
+ </h2> </div>
+
+</div>
+
+<div class="container m-0 mt-5">
+ 
+    <img style="width:174%; margin-left:-2%" src="pied.jpg">
+ 
+</div>
+
+<div class="container backgroundBande mt-3 "  >
+  <div class="row-full shadow mt-0">
+      <span class="text-center "> 
+        
+          <div class="col-12 pt-4 ">
+          
+            <h1 class="fontbandeau" >Nos compétences</h1>
+            
+          </div>
+        </div>
+      
+      </span>
+    </div>
+</div>
+
+<div class="container card-body m-0 mt-5">
+  <div class="row">
+  <div class="col-6 ml-5">
+        <h1 class="card-title" style="text-align: center;"> Recherche </h1>
+        <h4 class="card-content" style="text-align: justify;">Le parcours Recherche nécessite des étudiants ayant soif de curiosité, de recherche, d’approfondissement et d’interrogation. 
+        Les étudiants sont amenés à faire de la  traduction et relecture d’articles, mais apportent également un soutien logistique à des colloques. 
+</h4>
+<h4 class="card-content" style="text-align: justify;">
+Les étudiants apprennent les théories littéraires, l’analyse du discours et de nombreuses compétences linguistiques. La finalité de cette spécialité est de savoir rédiger des comptes-rendus et des synthèses, conduire une réflexion théorique sur la base de séminaires.
+Ce parcours se destine aux étudiants voulant être spécialistes de civilisation / culture, métiers de l’enseignement et de la recherche. 
+</h4>
+    </div> 
+    <div class="col-5">
+    <video style="width: 100%; height: 100%; margin: left 1600px;" src="recherche.mp4" autoplay loop muted></video>
+    </div>
+  </div>
+   
+</div>
+
+</div>
+
+<section>
+    <div class="container mt-5" >
+    	<div class="row mt-5"  >
+    	    
+    		<div class="col-md-4 mt-5" >
+    		    <div class="card profile-card-a">
+    		        <img src="img/Fond.png" alt="profile-sample1" class="background"/>
+    		        <img src="img/pp1.jpg" alt="profile-image" class="profile"/>
+                    <div class="card-content">
+                    <h2>Juliette Montillot<small>secrétaire </small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"></i></a></div>
+                    </div>
+                </div>
+               
+    		</div>
+    		
+    		<div class="col-md-4" >
+    		    <div class="card profile-card-a">
+    		        <img src="img/Fond.png" alt="profile-sample1" class="background"/>
+    		        <img src="img/pp3.jpg" alt="profile-image" class="profile"/>
+                    <div class="card-content ">
+                    <h2>Lou-Anne Grenouillet<small>Présidente</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> </i></a></div>
+                    </div>
+                </div>
+                
+    		</div>
+    		
+    		<div class="col-md-4 mt-5" >
+    		    <div class="card profile-card-a">
+    		        <img src="img/Fond.png" alt="profile-sample1" class="background"/>
+    		        <img src="img/pp2.jpg" alt="profile-image" class="profile"/>
+                    <div class="card-content">
+                    <h2>Coralie Adjam <small>trésorière</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+        </div>
+    </div>
+</div>
+
+
 <?php include "inc/footer.php"; ?>
 
